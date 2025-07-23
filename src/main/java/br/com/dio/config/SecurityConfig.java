0be4fe2a9e6a32dbf69205d2ebca258eb86c6609
AlexandreDresch <connectWithAlexandre@proton.me>
2025-07-22 21:23:33 -0300

    feat: Configure Spring Security for basic authentication and CSRF disablement

---
+++ src/main/java/br/com/dio/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package br.com.dio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(antMatcher("/public/**")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> {})
+                .httpBasic(httpBasic -> {})
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
